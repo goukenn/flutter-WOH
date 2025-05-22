93d90ae95a0236d680e29cfc1ee40c08257af10c
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2025-05-23 00:37:47 +0200

    check

--- lib/app/modules/settings/controllers/WOHAddressController.dart
+++ lib/app/modules/settings/controllers/WOHAddressController.dart
@@ -1,14 +1,11 @@
-// ignore_for_file:avoid_init_to_null,avoid_print,constant_identifier_names,file_names,no_leading_underscores_for_local_identifiers,non_constant_identifier_names,overridden_fields,prefer_collection_literals,prefer_interpolation_to_compose_strings,unnecessary_new,unnecessary_this,unused_local_variable
+// ignore_for_file:avoid_init_to_null,avoid_print,constant_identifier_names,file_names,no_leading_underscores_for_local_identifiers,non_constant_identifier_names,overridden_fields,prefer_collection_literals,prefer_interpolation_to_compose_strings,unnecessary_new,unnecessary_this,unused_local_variable, unused_field
 import 'package:get/get.dart';
 
-import '../../../../common/WOHUi.dart';
 import '../../../models/WOHAddressModel.dart';
 import '../../../repositories/WOHSettingRepository.dart';
-import '../../../services/WOHAuthService.dart';
-import '../../../services/WOHMyAuthService.dart';
 
 class WOHAddressController extends GetxController {
-  WOHSettingRepository _settingRepository;
+  late WOHSettingRepository _settingRepository;
   final addresses = <WOHAddressModel>[].obs;
 
   WOHAddressController() {
